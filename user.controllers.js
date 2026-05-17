4d5a0eaaf291d67b122ac703a7bfe1fa00fdb312
.Muhammad Habeel Ghaffar <habeel633@gmail.com>
2026-05-17 01:17:59 -0700

    Add files via upload

---
+++ user.controllers.js
@@ -0,0 +1,34 @@
+import User from "../models/user.model.js"
+import { v2 as cloudinary } from 'cloudinary';
+export const getCurrentUser=async (req,res)=>{
+    try {
+        const userId=req.userId
+        const user=await User.findById(userId).select("-password")
+        if(!user){
+            return res.status(400).json({message:"User not found"})
+        }
+
+        return res.status(200).json(user)
+    } catch (error) {
+        return res.status(400).json({message:"get current user error"})
+    }
+}
+
+export const updateAssistant=async (req,res)=>{
+    try {
+        const {assistantName,imageUrl}=req.body
+        let assistantImage;
+        if(req.file){
+            assistantImage=await uploadOnCloudinary(req.file.path)
+        }else{
+            assistantImage=imageUrl
+        }
+
+        const user=await User.findByIdAndUpdate(req.userId,{
+            AssistantName: assistantName,
+        },{new:true}).select("-password")
+        return res.status(200).json(user)
+    }catch (error) {
+    return res.status(400).json({message:"updateassistantError user error"})
+}
+}
